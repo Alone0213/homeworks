cf7e796062f7b11937a4392c3f36b62a1ba2aed2
Alone0213 <wangjiaxuan2017@163.com>
2025-11-17 08:51:41 +0800

    闲的

--- java/Palindrome.java
+++ java/Palindrome.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         String a;
-        Scanner input = new Scanner(System.in);
-        a=input.next();
-        int len=a.length();
-        for(int i=len-1; i>=0; i--){
-            char c=a.charAt(i);
-            System.out.printf("%c",c);
+        try (Scanner input = new Scanner(System.in)) {
+            a=input.next();
+            int len=a.length();
+            for(int i=len-1; i>=0; i--){
+                char c=a.charAt(i);
+                System.out.printf("%c",c);
+            }
         }
-        input.close();
     }
 }
