232b81f02414a23f0a008fdbc211d662cc8a20c0
Alone0213 <wangjiaxuan2017@163.com>
2025-11-19 16:41:11 +0800

    temp-update

---
+++ java/classdemo.java
@@ -0,0 +1,16 @@
+public class classdemo {
+    public class Car{
+        private String ownerName;
+        private double carSpeed;
+        private double carDirection;
+        public Car(String name){
+            ownerName=name;
+            carSpeed=0;
+            carDirection=0;
+        }
+        
+    }
+    public static void main(String[] args){
+
+    }
+}
