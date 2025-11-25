199c63d990b3a62cd562658a13bcde29a5e003e0
Alone0213 <Xiaoling021306@outlook.com>
2025-11-25 23:36:55 +0800

    矩阵转置

--- data_structure/Experiment_6.cpp
+++ data_structure/Experiment_6.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
+int n, m;//行与列
 int in;
 struct node
 {
@@ -9,7 +9,10 @@ struct node
     int val;
 };
 vector<node> t;
+vector<node> s;
 int cnt;
+vector<int> num;//原矩阵中每列非零元的个数
+vector<int> pospercol;
 signed main()
 {
     printf("请输入行数和列数，两者中间空一格：\n");
@@ -23,9 +26,27 @@ signed main()
             if (in)
             {
                 t.push_back({i, j, in});
+                num[j]++;
             }
         }
     }
-
+    pospercol[1]=1;
+    for(int i=2; i<=m; i++){
+        pospercol[i]=pospercol[i-1]+num[i-1];
+    }
+    for(auto i:t){
+        int pos=pospercol[i.c];
+        s[pos].c=i.r;
+        s[pos].r=i.c;
+        s[pos].val=i.val;
+        pospercol[i.c]++;
+    }
+    for(auto i:t){
+        printf("%d %d %d\n",i.r,i.c,i.val);
+    }
+    puts("");
+    for(auto i:s){
+        printf("%d %d %d\n",i.r,i.c,i.val);
+    }
     return 0;
 }
