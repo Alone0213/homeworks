0e4b06352675ab974f1e586bcaae3a2aafebcacd
Alone0213 <wangjiaxuan2017@163.com>
2025-12-02 22:14:50 +0800

    哈夫曼树

---
+++ data_structure/Experiment_11.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int ls,rs,val,num;
+    bool operator<(const node &A) const{
+        return val>A.val;
+    }
+}tr[10<<2];
+int cnt;
+priority_queue<node> q;
+void print(int s){//
+    printf("number:%d value:%d\n",tr[s].num,tr[s].val);
+    printf("node[%d]`s left son: ",tr[s].num);
+    if(tr[s].ls) print(tr[s].ls);
+    else printf("NULL\n");
+    printf("node[%d]`s right son: ",tr[s].num);
+    if(tr[s].rs) print(tr[s].rs);
+    else printf("NULL\n");
+    return;
+}
+signed main(){
+    for(int i=1; i<=4; i++){
+        tr[++cnt]=node{0,0,i,i};
+        q.push(tr[i]);
+    }
+    while(q.size()>1){
+        node m1=q.top();q.pop();
+        node m2=q.top();q.pop();
+        tr[++cnt]=node{m1.num,m2.num,m1.val+m2.val,cnt};
+        q.push(tr[cnt]);
+    }
+    print(cnt);
+    return 0;
+}
---
+++ data_structure/test.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+int ep[100];
+int ba[100];
+int cnt;
+signed main(){
+    cin >> a;
+    printf("%d\n",a);
+}
